f0b43afab529330dbafe1ac2856edee0112ab0d5
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-10-17 18:09:28 +0500

    create project

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
