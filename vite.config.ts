66a6e9d6266cc11e1637997a26db2205f0ff4a6a
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-10-20 21:46:34 +0500

    fix gh-pages build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "apricode-test",
   resolve: {
     alias: {
       src: "/src",
